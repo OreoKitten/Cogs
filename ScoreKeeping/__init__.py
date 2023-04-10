8f5035128543a2b07c5bc993303fe6867391f2c7
Adl G <oreocatadl@gmail.com>
2023-04-10 16:58:53 -0400

    Update __init__.py

--- ScoreKeeping/__init__.py
+++ ScoreKeeping/__init__.py
@@ -3,7 +3,7 @@ from pathlib import Path
 
 from redbot.core.bot import Red
 
-from .scorekeeping import Scorekeeping
+from .Scorekeeping.scorekeeping import Scorekeeping
 
 with open(Path(__file__).parent / "info.json") as fp:
     __red_end_user_data_statement__ = json.load(fp)["end_user_data_statement"]
