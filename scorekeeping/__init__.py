024238f4cbae02d8385f393192dcd13b3e9e36ae
Adl G <oreocatadl@gmail.com>
2023-04-10 17:21:18 -0400

    Update __init__.py

--- scorekeeping/__init__.py
+++ scorekeeping/__init__.py
@@ -9,4 +9,4 @@ with open(Path(__file__).parent / "info.json") as fp:
     __red_end_user_data_statement__ = json.load(fp)["end_user_data_statement"]
 
 def setup(bot):
-    bot.add_cog(Scorekeeping(bot))
+    bot.add_cog(scorekeeping(bot))
